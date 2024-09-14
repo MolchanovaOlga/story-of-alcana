eca96fe24d8199979568c1ba7873179a463c99b6
MolchanovaOlga <designer.molchanova@gmail.com>
2024-09-14 20:28:32 +0300

    Change scroll for navigations

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { initFaq } from './js/faq.js';
 import { initSlider } from './js/slider.js';
 import './js/mobile-menu.js';
+import './js/scroll-reload.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initFaq();
