c63c9600de8e267ab4a3d7772cb3219e67d9e29c
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2024-09-14 12:04:43 +0300

    Fix style

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { initFaq } from './js/faq.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOM fully loaded and parsed'); // Лог для перевірки завантаження DOM
   initFaq();
 });
