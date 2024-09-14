ee41a5aff075f8f1c7eb3b3dd82d0abdd3a6fa8f
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2024-09-15 02:25:08 +0300

    Refactor and add slider

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { initFaq } from './js/faq.js';
 import { initSlider } from './js/slider.js';
+import { initReviewsSlider } from './js/reviews.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initFaq();
   initSlider();
+  initReviewsSlider();
 });
